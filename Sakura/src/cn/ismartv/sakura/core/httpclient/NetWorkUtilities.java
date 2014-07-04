70559f5f5b7c39822a203f9afb979c6fd1be11cc
fenghuibin <fenghuibin@ismartv.cn>
2014-07-04 17:34:43 +0800

    add network utli

--- Sakura/src/cn/ismartv/sakura/core/httpclient/NetWorkUtilities.java
+++ Sakura/src/cn/ismartv/sakura/core/httpclient/NetWorkUtilities.java
@@ -1,8 +1,16 @@
 package cn.ismartv.sakura.core.httpclient;
 
+import android.content.Entity;
+import android.util.Log;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 /**
  * Created by fenghb on 14-7-4.
@@ -15,10 +23,25 @@ public class NetWorkUtilities {
 
     public static void getNodeList() {
         HttpClient client = new DefaultHttpClient();
-        HttpGet get = new HttpGet();
+        HttpGet get = new HttpGet(HOST + GET_NODE_URL);
+        HttpResponse response = null;
+        try {
+            response = client.execute(get);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            HttpEntity entity = response.getEntity();
+            String result = new String();
+            try {
+                result = EntityUtils.toString(entity);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, result);
+        } else {
+            Log.e(TAG,response.getStatusLine().toString());
+        }
     }
-
-
 }
---
+++ Sakura/src/ismartv/cn/sakura/test/core/HttpClientTest.java
@@ -0,0 +1,26 @@
+package ismartv.cn.sakura.test.core;
+
+import android.test.AndroidTestCase;
+import cn.ismartv.sakura.core.httpclient.NetWorkUtilities;
+
+/**
+ * Created by fenghb on 14-7-4.
+ */
+public class HttpClientTest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getNodeList();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    private void getNodeList() {
+        NetWorkUtilities.getNodeList();
+
+    }
+}
