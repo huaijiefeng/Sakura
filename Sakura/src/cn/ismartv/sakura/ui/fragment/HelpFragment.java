35c3870d87b6a189561efbd14032004cf425ba10
fenghuibin <fenghuibin@ismartv.cn>
2014-07-28 14:53:04 +0800

    test

--- Sakura/src/cn/ismartv/sakura/ui/fragment/HelpFragment.java
+++ Sakura/src/cn/ismartv/sakura/ui/fragment/HelpFragment.java
@@ -1,12 +1,14 @@
 package cn.ismartv.sakura.ui.fragment;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import cn.ismartv.sakura.R;
 import cn.ismartv.sakura.ui.activity.HelpDetailActivity;
 
--- Sakura/src/cn/ismartv/sakura/ui/widget/progressbar/SakuraProgressBar.java
+++ Sakura/src/cn/ismartv/sakura/ui/widget/progressbar/SakuraProgressBar.java
@@ -1,7 +1,22 @@
 package cn.ismartv.sakura.ui.widget.progressbar;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
 /**
  * Created by fenghb on 14-7-28.
  */
-public class SakuraProgressBar {
+public class SakuraProgressBar extends ProgressBar {
+    public SakuraProgressBar(Context context) {
+        super(context);
+    }
+
+    public SakuraProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SakuraProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 }
